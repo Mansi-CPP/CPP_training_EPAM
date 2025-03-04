b48afbba30e5f1344559ebf4682657f2cc06a274
Mansi-CPP <mansi_shahi@epam.com>
2025-03-04 14:24:33 +0530

    Update compile_time.cpp

--- c++_OOPS/compile_time.cpp
+++ c++_OOPS/compile_time.cpp
@@ -10,3 +10,19 @@ double Bank_account::balance(double rate)
 	this->rate = rate;
 	return amount+((amount*rate*time)/100);
 }
+
+#include <iostream>
+#include<string >
+#include "C_poly.h"
+using namespace std;
+
+int main() {
+	Bank_account b1;
+	b1.amount= 1000;
+	b1.time = 1;
+	int r = 10;
+	cout << "original amount" << b1.balance();
+	cout << "interest amount" << b1.balance(r);
+
+	return 0;
+}
