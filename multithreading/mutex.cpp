bdc3f12692d1dbaa3e0a6953856873caaf2d8d25
Mansi-CPP <mansi_shahi@epam.com>
2025-04-21 14:42:15 +0530

    Create mutex.cpp

---
+++ multithreading/mutex.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include<thread>
+#include<chrono>
+#include<mutex>
+ 
+using namespace std;
+int balance = 0;
+mutex m;
+void add() {
+	for (int i = 0; i < 1000000; i++) {
+		balance++;
+	}
+}
+int balance2 = 0;
+void add2() {
+	//critical section
+	m.lock();
+	for (int i = 0; i < 1000000; i++) {
+		balance2++;
+	}
+	m.unlock();
+}
+int main() {
+	thread t1(add);
+	thread t2(add);  
+	t1.join();
+	t2.join();
+	cout << "balance" << balance<<endl;
+	// with mutex
+	thread t3(add2);
+	thread t4(add2);  
+	t3.join();
+	t4.join();
+	cout << "balance" << balance2 << endl;
+	return 0;
+}
+//output 1
+//balance1449902 without mutex
+//balance2000000 with mutex
+// 
+//output 2
+//balance1333417
+//balance2000000
