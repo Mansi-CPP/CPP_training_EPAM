837ea6e5364d8fc1a8c45a70f1a2879d4fb1fe08
Mansi-CPP <mansi_shahi@epam.com>
2025-06-17 07:59:14 +0530

    Update exception.cpp

--- exceptionhandling/exception.cpp
+++ exceptionhandling/exception.cpp
@@ -1,1 +1,42 @@
 #include<iostream>
+#include<exception>
+#include<array>
+using namespace std;
+//int divide(int a, int b) {
+//	try {
+//		if (b == 0) {
+//			throw invalid_argument("division by zero");
+//		}
+//	}
+//	catch (const invalid_argument& e) {
+//		cout << "divide"<<e.what() << endl;
+//		throw;
+//	}
+//}
+class invaliddeposit :public exception {
+	const char* what() const noexcept  override{
+		return "Invalid deposit";
+	}
+};
+class atm {
+	int amount = 8000;
+public:
+	void deposit(int a) {
+		if (a < 0) {
+			throw invaliddeposit();
+	    }
+		amount += a;
+	}
+};
+int main() {
+	try {
+		//divide(2, 0);
+		atm a;
+		a.deposit(-10);
+	}
+	catch (const exception& e) {
+		cout <<"Main"<< e.what() << endl;
+	}
+	return 0;
+
+}
